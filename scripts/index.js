88f46b9968bc4e6814645f971bdd1d611eeb1e80
Antonio <asoriano@appdev.yearup.org>
2024-05-17 13:22:54 -0500

    completed lottery_tickets assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 "use strict"
 
 let winningTickets = [
-    {tixNum: "1001001", expires: "2022-09-05", prize: 100000},
-    {tixNum: "1298711", expires: "2022-10-10" , prize: 250000},
-    {tixNum: "1309182", expires: "2022-12-30" , prize: 500000},
-    {tixNum: "1456171", expires: "2023-01-20", prize: 1000000},
-    {tixNum: "3332871", expires: "2022-05-20", prize: 1000000},
-    {tixNum: "4651529", expires: "2022-12-15", prize: 100000},
-    {tixNum: "5019181", expires: "2023-01-31", prize: 250000},
-    {tixNum: "5168261", expires: "2023-03-01", prize: 1000000},
-    {tixNum: "6761529", expires: "2022-12-15", prize: 250000},
-    {tixNum: "7778172", expires: "2023-01-15", prize: 5000000},
-    {tixNum: "8751426", expires: "2020-09-15", prize: 100000}
+    { tixNum: "1001001", expires: "2022-09-05", prize: 100000 },
+    { tixNum: "1298711", expires: "2022-10-10", prize: 250000 },
+    { tixNum: "1309182", expires: "2022-12-30", prize: 500000 },
+    { tixNum: "1456171", expires: "2023-01-20", prize: 1000000 },
+    { tixNum: "3332871", expires: "2022-05-20", prize: 1000000 },
+    { tixNum: "4651529", expires: "2022-12-15", prize: 100000 },
+    { tixNum: "5019181", expires: "2023-01-31", prize: 250000 },
+    { tixNum: "5168261", expires: "2023-03-01", prize: 1000000 },
+    { tixNum: "6761529", expires: "2022-12-15", prize: 250000 },
+    { tixNum: "7778172", expires: "2023-01-15", prize: 5000000 },
+    { tixNum: "8751426", expires: "2020-09-15", prize: 100000 }
 ];
 
 window.onload = () => {
@@ -20,6 +20,34 @@ window.onload = () => {
 
 }
 
-function loadWinnningTicketsTable(){
+function loadWinnningTicketsTable() {
 
+    // I need to get a hold of the element with ID of winningTicketsTblBody
+    let tableBody = document.querySelector("#winningTicketsTblBody");
+
+    // need to work with all the lottery tickets
+    winningTickets.forEach((ticket) => {
+
+        // call a function to build the individual table rows and data
+        buildTableRow(tableBody, ticket);
+
+    });
+
+    // this function is responsible for building and adding a table row to the table body
+    function buildTableRow(tableBody, data) {
+
+        // you could pass a -1 into insertRow, but when not given an argument, the default index value is -1
+        let newRow = tableBody.insertRow();
+
+        // you could pass a 0 into insertCell, but when not given an argument, the default index value is 0
+        let cell1 = newRow.insertCell();
+        cell1.innerHTML = data.tixNum;
+
+        let cell2 = newRow.insertCell();
+        cell2.innerHTML = `$${data.prize.toFixed(2)}`;
+
+        let cell3 = newRow.insertCell();
+        cell3.innerHTML = new Date(data.expires).toLocaleDateString();
+
+    }
 }
